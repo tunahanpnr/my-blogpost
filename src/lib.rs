3f1ccd05a0f9cf16df81f0ec089aa3309f7a610d
Tunahan Pinar <tunahanpnr1@gmail.com>
2024-11-03 13:31:39 +0100

    feat: implement blog schemas

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod routes;
 pub mod handlers;
 pub mod models;
+pub mod schemas;
---
+++ src/schemas/blog.rs
@@ -0,0 +1,18 @@
+use serde::{Deserialize, Serialize};
+
+pub struct CreateBlogRequest {
+    pub username: String,
+    pub text: String,
+    pub avatar_path: Option<String>,
+    pub image_path: Option<String>,
+    pub created_at: chrono::DateTime<chrono::Utc>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct BlogResponse {
+    pub username: String,
+    pub text: String,
+    pub avatar_path: Option<String>,
+    pub image_path: Option<String>,
+    pub created_at: chrono::DateTime<chrono::Utc>
+}
