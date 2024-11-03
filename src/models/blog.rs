39f34c866eeae3b888e6946155b988ea26ac1297
Tunahan Pinar <tunahanpnr1@gmail.com>
2024-11-03 13:28:11 +0100

    feat: implement blog model

---
+++ src/models/blog.rs
@@ -0,0 +1,12 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize, sqlx::FromRow)]
+#[allow(non_snake_case)]
+pub struct BlogModel {
+    pub id: String,
+    pub username: String,
+    pub text: String,
+    pub avatar_path: Option<String>,
+    pub image_path: Option<String>,
+    pub created_at: chrono::DateTime<chrono::Utc>,
+}
