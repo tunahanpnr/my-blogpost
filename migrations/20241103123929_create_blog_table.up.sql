b5bae1ae09e8454e89e923d8760950d2fc21cc2b
Tunahan Pinar <tunahanpnr1@gmail.com>
2024-11-04 10:36:34 +0100

    fix: update sql migration

--- migrations/20241103123929_create_blog_table.up.sql
+++ migrations/20241103123929_create_blog_table.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS blog (
     id CHAR(36) PRIMARY KEY NOT NULL,
     username TEXT NOT NULL,
     text TEXT NOT NULL,
-    image_path TEXT,
-    avatar_path TEXT,
+    image TEXT,
+    avatar TEXT,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
